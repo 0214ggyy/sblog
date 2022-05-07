d1509c4410d08b8f1e3a5d28b65bba6a74c3e73f
gsj <930905015@qq.com>
2022-05-07 16:25:24 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+const { defaultTheme } = require('vuepress')
+// const { docsearchPlugin } = require('@vuepress/plugin-docsearch')
+const { searchPlugin } = require('@vuepress/plugin-search')
+module.exports = {
+  plugins: [
+    searchPlugin({
+      // 配置项
+    }),
+  ],
+  theme: defaultTheme({
+    // 在这里进行配置
+    // search: true,
+    // searchMaxSuggestions: 10,
+    logo: '/images/logo.jpg',
+    repo: 'https://gitee.com/Dirp',
+    navbar: [
+      {
+        text: '前端',
+        link: '/sb/web/',
+      },
+      {
+        text: '后端',
+        link: '/sb/backend/',
+      },
+    ],
+  })
+}
